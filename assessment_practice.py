b18f3a27c7c346a870a4e1a33de1e43781b9a084
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-12 10:47:00 +0000

    more tasks

--- assessment_practice.py
+++ assessment_practice.py
@@ -9,6 +9,17 @@ my_tuple = 1,2,3,4,5
 print(my_tuple)
 # can you append to this? NO 
 
-# Change this to a dict:
+# print tuple up to number 3
 my_tuple = 1,2,3,4,5
 print(my_tuple[0:3]) # not it's not inclusive of the last index, so this only prints to index number 2
+
+# declare a dictionary of a shopping list
+shopping_list = {
+    'fruits': 5.00,
+    'eggs' : 2.50,
+    'veg' : 8.99
+}
+print(type(shopping_list))
+
+# print the price of the third item 
+print(shopping_list['veg'])
