4dc22fdcc3b91a1e92e5698292df7535b780d02e
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-12 10:34:59 +0000

    second q

--- assessment_practice.py
+++ assessment_practice.py
@@ -3,3 +3,12 @@ num_list = [1,2,3,4,5]
 print(num_list)
 num_list.append(6)
 print(num_list)
+
+# create  a tuple  with values 1 - 5
+my_tuple = 1,2,3,4,5
+print(my_tuple)
+# can you append to this? NO 
+
+# Change this to a dict:
+my_tuple = 1,2,3,4,5
+print(my_tuple[0:3]) # not it's not inclusive of the last index, so this only prints to index number 2
